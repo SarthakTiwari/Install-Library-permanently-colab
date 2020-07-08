dc1da01539419c723fe9d9b605c43a0be981bb99
Sarthak Tiwari <42897110+SarthakTiwari@users.noreply.github.com>
2020-07-08 19:09:15 +0530

    Update pytorch_lightning_loading.py

--- pytorch_lightning_loading.py
+++ pytorch_lightning_loading.py
@@ -5,4 +5,6 @@ nb_path = '/content/notebooks'
 os.symlink('/content/drive/My Drive/Colab Notebooks', nb_path)
 sys.path.insert(5, nb_path)
 
+!pip install --target=$nb_path pytorch_lightning #call only once
+
 import pytorch_lightning
